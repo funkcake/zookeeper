d7fd6cab53e909647cef7091ca85a1f75c546ea8
funkcake <castobrianj@gmail.com>
2021-04-22 16:52:15 -0500

    now Done with mod 3

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 const path = require('path');
+
 const express = require('express');
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.static('public'));
+// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -89,6 +93,7 @@ app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
+  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
